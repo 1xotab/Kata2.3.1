9cb8deb4c0f5f275527e866975d5b46ec474afda
1xotab <ninetonine319@gmail.com>
2022-04-07 18:39:13 +0300

    Mistakes fix (07.04.2022)

--- src/main/java/web/CarService.java
+++ src/main/java/web/CarService.java
@@ -9,18 +9,18 @@ import web.model.Car;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@Service
 public class CarService {
 
-    public List getCars(int quantity) {
-        List<Car> cars = new ArrayList<>();
+    List<Car> cars = new ArrayList<>();
 
-        Car car1 = new Car(1, 22151, "bmv");
-        Car car2 = new Car(2, 52153, "dodge");
-        Car car3 = new Car(3, 62613, "mercedes");
-        Car car4 = new Car(4, 31253, "mazda");
-        Car car5 = new Car(5, 78513, "lada");
+    Car car1 = new Car(1, 22151, "bmv");
+    Car car2 = new Car(2, 52153, "dodge");
+    Car car3 = new Car(3, 62613, "mercedes");
+    Car car4 = new Car(4, 31253, "mazda");
+    Car car5 = new Car(5, 78513, "lada");
 
+    public List<Car> getCars(int quantity) {
         cars.add(car1);
         cars.add(car2);
         cars.add(car3);
