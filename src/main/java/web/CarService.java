28820c2e1a5ff9bcadff10f4c9c848861c968868
1xotab <ninetonine319@gmail.com>
2022-04-13 00:38:00 +0300

    Mistakes fix (13.04.2022)

--- src/main/java/web/CarService.java
+++ src/main/java/web/CarService.java
@@ -12,22 +12,14 @@ import java.util.List;
 @Service
 public class CarService {
 
-    List<Car> cars = new ArrayList<>();
+    List<Car> cars = List.of(
+            new Car(1, 22151, "bmv"),
+            new Car(2, 52153, "dodge"),
+            new Car(3, 62613, "mercedes"),
+            new Car(4, 31253, "mazda"),
+            new Car(5, 78513, "lada"));
 
     public List<Car> getCars(int quantity) {
-
-        Car car1 = new Car(1, 22151, "bmv");
-        Car car2 = new Car(2, 52153, "dodge");
-        Car car3 = new Car(3, 62613, "mercedes");
-        Car car4 = new Car(4, 31253, "mazda");
-        Car car5 = new Car(5, 78513, "lada");
-
-        cars.add(car1);
-        cars.add(car2);
-        cars.add(car3);
-        cars.add(car4);
-        cars.add(car5);
-
         return cars.stream().limit(quantity).toList();
     }
 }
